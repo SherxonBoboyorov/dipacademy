9148a264e1ab1201dcc492bf64854d2c6ffba821
SherxonBoboyorov <sherxonboboyorov@gmail.com>
2023-02-14 17:42:48 +0500

    uzgarishlar

--- resources/views/front/command.blade.php
+++ resources/views/front/command.blade.php
@@ -4,13 +4,13 @@
 
     <!-- academy start -->
 
-    <div class="academy" style="background-image:url(foto/academy_fon.png)">
+    <div class="academy" style="background-image:url({{ asset('front/foto/academy_fon.png') }})">
         <section class="container">
             <div class="academy__cart">
                 <h2 class="academy__title__h2">Команда</h2>
                 <ul class="academy__menu">
                     <li>
-                        <a href="index.html" class="academy__menu__link">Main</a>
+                        <a href="{{ route('/') }}" class="academy__menu__link">Main</a>
                     </li>
                     <li>
                         <a href="command_in.html" class="academy__menu__link">КОМАНДА</a>
@@ -33,31 +33,32 @@
 
                     <div class="leadership__item__list">
 
+
                         <div class="leadership__img">
-                            <img src="foto/leadership_1.png" alt="leadership">
+                            <img src="{{ asset($team->image) }}" alt="leadership">
                         </div>
 
                         <section class="leadership__item__text">
-                            <h2 class="leadership__title__h2">John doe</h2>
+                            <h2 class="leadership__title__h2">{{ $team->{'name_' . app()->getLocale()} }}</h2>
                             <ul class="leadership__menu__contacts">
                                 <li>
                                     <span>Должность:</span>
-                                    <h4 class="leadership__link__contacts">Ректор</h4>
+                                    <h4 class="leadership__link__contacts">{{ $team->{'job_title_' . app()->getLocale()} }}</h4>
                                 </li>
 
                                 <li>
                                     <span>Телефон:</span>
-                                    <a href="tel:(+998 71) 123-45-67" class="leadership__link__contacts">(+998 71) 123-45-67</a>
+                                    <a href="tel:{{ $team->phone_number }}" class="leadership__link__contacts">{{ $team->phone_number }}</a>
                                 </li>
 
                                 <li>
                                     <span>Дни приема:</span>
-                                    <h4 class="leadership__link__contacts">Четверг - 10:00-11:00</h4>
+                                    <h4 class="leadership__link__contacts">{{ $team->{'reception_days_' . app()->getLocale()} }}</h4>
                                 </li>
 
                                 <li>
                                     <span>Электронная почта:</span>
-                                    <a href="mailto:info@doe.com" class="leadership__link__contacts">info@doe.com</a>
+                                    <a href="mailto:{{ $team->email }}" class="leadership__link__contacts">{{ $team->email }}</a>
                                 </li>
                             </ul>
                         </section>
@@ -65,11 +66,10 @@
                     </div>
 
                     <section class="leadership__text__next">
-                        <h2 class="about__title__h2">Lorem ipsum</h2>
+                        {{-- <h2 class="about__title__h2">Lorem ipsum</h2> --}}
                         <div class="academy_contint__text clearfix">
                             <p>
-                                Sed ut perspiciatis, unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam eaque ipsa, quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt, explicabo. Nemo enim ipsam voluptatem, quia voluptas sit, aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos, qui ratione voluptatem sequi nesciunt, neque porro quisquam est, qui dolorem ipsum, quia dolor sit, amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt, ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur?
-                                Quis autem vel eum iure reprehenderit, qui in ea voluptate velit esse, quam nihil molestiae consequatur, vel illum, qui dolorem eum fugiat, quo voluptas nulla pariatur? At vero eos et accusamus et iusto odio dignissimos ducimus, qui blanditiis praesentium voluptatum deleniti atque corrupti, quos dolores et quas molestias excepturi sint, obcaecati cupiditate non provident, similique sunt in culpa, qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio, cumque nihil impedit, quo minus id, quod maxime placeat, facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet, ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.
+                                {!! $team->{'description_' . app()->getLocale()} !!}
                             </p>
                         </div>
                     </section>
